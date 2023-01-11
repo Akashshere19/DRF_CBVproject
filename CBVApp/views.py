f8ab2ba49306298290f8f28441c42e1ce7410326
akashshere19 <akashshere19@gmail.com>
2023-01-11 20:39:01 +0530

    add post

--- CBVApp/views.py
+++ CBVApp/views.py
@@ -2,23 +2,22 @@ from django.shortcuts import render
 from CBVApp.models import Course,CourseSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
-
+from rest_framework import status
 
 # Create your views here.
-
-
-
-
-
-
 
 class CourseListView(APIView):
     def get(self,request):
         courses =  Course.objects.all()
         serializers = CourseSerializer(courses,many=True)
-        
         return Response(serializers.data)
 
 
+    def post(self,request):
+        CourseSerializers = CourseSerializer(data = request.data)
+        if CourseSerializers.is_valid():
+            CourseSerializers.save()
+            return Response(CourseSerializers.data,status = status.HTTP_201_CREATED)
+
+        return Response(CourseSerializers.errors)    
 
